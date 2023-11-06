8b595d24003c53979f37f4fcccdd7504aa4fdfb0
amrsallam10 <amrsallam1000@gmail.com>
2023-11-06 20:05:17 +0200

    finalized the new version of the election algo [classic bully]

--- src/server.rs
+++ src/server.rs
@@ -27,8 +27,8 @@ use std::collections::HashSet;
 // struct to hold server states (modify as needed)
 #[derive(Clone)]
 struct ServerStats {
-    elections_initiated_by_me: HashMap<String, (u32, u32)>, // req_id -> (own_p, #anticipated Oks)
-    elections_received_oks: HashMap<String, HashMap<String, u32>>, // req_id -> #currently received Oks
+    elections_initiated_by_me: HashSet<String>, // req_id -> (own_p, #anticipated Oks)
+    elections_received_oks: HashSet<String>,    // req_id -> #currently received Oks
     elections_leader: HashMap<String, (String, u32)>, // req_id -> (addr of currently chosen leader, its priority)
     running_elections: HashMap<String, u32>,
     requests_buffer: HashMap<String, Msg>,
@@ -51,26 +51,18 @@ struct Msg {
     payload: Option<String>,
 }
 
-async fn handle_ok_msg(
-    req_id: String,
-    src_addr: SocketAddr,
-    p: u32,
-    socket: Arc<&UdpSocket>,
-    stats: Arc<Mutex<ServerStats>>,
-) {
+async fn handle_ok_msg(req_id: String, stats: Arc<Mutex<ServerStats>>) {
     let mut data = stats.lock().await;
-    data.elections_received_oks
-        .entry(req_id.clone())
-        .or_insert_with(HashMap::new)
-        .insert(src_addr.to_string(), p);
+    data.elections_received_oks.insert(req_id);
 }
 
 async fn send_ok_msg(
     req_id: String,
     src_addr: std::net::SocketAddr,
-    socket: Arc<&UdpSocket>,
+    socket: Arc<UdpSocket>,
     stats: Arc<Mutex<ServerStats>>,
 ) {
+    println!("sending ok msg");
     let data = stats.lock().await;
     let own_priority = *data.running_elections.get(&req_id).unwrap();
     let msg = Msg {
@@ -90,24 +82,41 @@ async fn handle_election(
     p: u32,
     req_id: String,
     src_addr: std::net::SocketAddr,
-    socket: Arc<&UdpSocket>,
+    service_socket: Arc<UdpSocket>,
+    election_socket: Arc<UdpSocket>,
     stats: Arc<Mutex<ServerStats>>,
 ) {
     let mut data = stats.lock().await;
-    if !data.running_elections.contains_key(&req_id) {
-        let own_priority = {
+    let own_priority = data
+        .running_elections
+        .entry(req_id.clone())
+        .or_insert_with(|| {
             let mut rng = rand::thread_rng();
-            rng.gen_range(1..1000)
-        } as u32;
-        data.running_elections.insert(req_id.clone(), own_priority);
-    }
-    if *data.running_elections.get(&req_id).unwrap() > p {
+            rng.gen_range(1..1000) as u32
+        });
+    println!("Own priority {} for {}", own_priority, req_id);
+    if *own_priority > p {
+        println!("Own priority is higher");
         drop(data);
-        send_ok_msg(req_id, src_addr, socket, stats).await;
+        send_ok_msg(
+            req_id.clone(),
+            src_addr,
+            election_socket.clone(),
+            stats.clone(),
+        )
+        .await;
+        send_election_msg(
+            service_socket.clone(),
+            election_socket.clone(),
+            stats.clone(),
+            req_id.clone(),
+            false,
+        )
+        .await;
     }
 }
 
-async fn reply_to_client(socket: Arc<&UdpSocket>, req_id: String, stats: Arc<Mutex<ServerStats>>) {
+async fn reply_to_client(socket: Arc<UdpSocket>, req_id: String, stats: Arc<Mutex<ServerStats>>) {
     let data = stats.lock().await;
     println!("Replying to Client");
     let addr = &socket.local_addr().unwrap().to_string();
@@ -119,29 +128,29 @@ async fn reply_to_client(socket: Arc<&UdpSocket>, req_id: String, stats: Arc<Mut
         .unwrap();
 }
 
-async fn handle_coordinator(
-    stats: Arc<Mutex<ServerStats>>,
-    coordinator_ip: String,
-    socket: Arc<&UdpSocket>,
-    req_id: String,
-) {
+async fn handle_coordinator(stats: Arc<Mutex<ServerStats>>, req_id: String) {
+    println!("Flushing related stats");
     let mut data = stats.lock().await;
-    let ips = data.sockets_ips.clone();
-    data.running_elections
-        .remove(&req_id)
-        .expect("Attempt to remove req_id that does not exist");
-    if coordinator_ip == ips.0 || coordinator_ip == ips.1 {
-        drop(data);
-        reply_to_client(socket, req_id, stats).await;
+    if data.running_elections.remove(&req_id).is_some() {
+        // Entry was removed (if it existed)
+    }
+
+    if data.elections_received_oks.remove(&req_id) {
+        // Entry was removed (if it existed)
+    }
+
+    if data.requests_buffer.remove(&req_id).is_some() {
+        // Entry was removed (if it existed)
     }
 }
 
 async fn broadcast_coordinator(
-    socket: Arc<&UdpSocket>,
+    socket: Arc<UdpSocket>,
     leader: String,
     peer_servers: Vec<(SocketAddr, SocketAddr)>,
     req_id: String,
 ) {
+    println!("broadcasting as a coordinator");
     for server in &peer_servers {
         let msg = Msg {
             sender: socket.local_addr().unwrap(),
@@ -157,62 +166,75 @@ async fn broadcast_coordinator(
     }
 }
 
-async fn initiate_election(
-    socket: Arc<&UdpSocket>,
+async fn send_election_msg(
+    service_socket: Arc<UdpSocket>,
+    election_socket: Arc<UdpSocket>,
     stats: Arc<Mutex<ServerStats>>,
     req_id: String,
+    init_f: bool,
 ) {
     let mut data = stats.lock().await;
+    if !init_f || !data.running_elections.contains_key(&req_id) {
+        let sender = election_socket.local_addr().unwrap();
+        println!("Sending Election msgs!");
 
-    let sender = socket.local_addr().unwrap();
-    println!("{} Initiating Election!", sender);
+        let peer_servers = data.get_peer_servers();
 
-    let peer_servers = data.get_peer_servers();
-    println!("Inserting Request {} in running elections", req_id);
-    let own_priority = {
-        let mut rng = rand::thread_rng();
-        rng.gen_range(1..1000)
-    } as u32;
-    println!("My own Priority {}", own_priority);
+        let own_priority = data
+            .running_elections
+            .entry(req_id.clone())
+            .or_insert_with(|| {
+                let mut rng = rand::thread_rng();
+                rng.gen_range(1..1000) as u32
+            });
+        println!("My own Priority {} for request {}", own_priority, req_id);
 
-    data.running_elections.insert(req_id.clone(), own_priority);
-    println!("Inserting Request {} in initiated elections", req_id);
+        for server in &peer_servers {
+            let msg = Msg {
+                sender,
+                receiver: server.1,
+                msg_type: Type::ElectionRequest(*own_priority),
+                payload: Some(req_id.clone()),
+            };
+            let serialized_msg = serde_json::to_string(&msg).unwrap();
+            election_socket
+                .send_to(serialized_msg.as_bytes(), server.1)
+                .await
+                .unwrap();
+        }
+        drop(data);
+        println!("Waiting for ok msg");
+        sleep(Duration::from_millis(1000)).await;
 
-    for server in &peer_servers {
-        let msg = Msg {
-            sender,
-            receiver: server.1,
-            msg_type: Type::ElectionRequest(own_priority),
-            payload: Some(req_id.clone()),
-        };
-        let serialized_msg = serde_json::to_string(&msg).unwrap();
-        socket
-            .send_to(serialized_msg.as_bytes(), server.1)
-            .await
-            .unwrap();
+        let data = stats.lock().await;
+
+        if !data.elections_received_oks.contains(&req_id) {
+            println!("Did not find ok msgs");
+            drop(data);
+            let own_ip = election_socket.local_addr().unwrap().to_string();
+            broadcast_coordinator(
+                election_socket.clone(),
+                own_ip,
+                peer_servers,
+                req_id.clone(),
+            )
+            .await;
+            reply_to_client(service_socket.clone(), req_id.clone(), stats.clone()).await;
+            handle_coordinator(stats.clone(), req_id.clone()).await;
+        }
     }
-
-    sleep(Duration::from_millis(20)).await;
-
-    let (coordinator_id, max_priority) = data
-        .elections_received_oks
-        .get(&req_id)
-        .and_then(|inner_map| inner_map.iter().max_by_key(|&(_, p)| p))
-        .map(|(ip, p)| (ip.to_owned(), *p))
-        .unwrap_or((sender.to_string(), own_priority));
-
-    broadcast_coordinator(socket, coordinator_id, peer_servers, req_id).await;
 }
 
 async fn handle_client(
     id: u32,
     msg: Msg,
-    socket: Arc<&UdpSocket>,
+    service_socket: Arc<UdpSocket>,
+    election_socket: Arc<UdpSocket>,
     stats: Arc<Mutex<ServerStats>>,
     src_addr: SocketAddr,
 ) {
     let req_id = format!("{}:{}", msg.sender, id);
-    println!("Handling Request {req_id}");
+    println!("Handling Request {}", req_id);
     let mut data = stats.lock().await;
     data.requests_buffer
         .entry(req_id.clone())
@@ -230,7 +252,7 @@ async fn handle_client(
 
         if !data.running_elections.contains_key(&req_id) {
             drop(data);
-            initiate_election(socket, stats, req_id).await;
+            send_election_msg(service_socket, election_socket, stats, req_id, true).await;
         }
     }
 }
@@ -238,7 +260,8 @@ async fn handle_client(
 async fn handle_request(
     buffer: &[u8],
     src_addr: std::net::SocketAddr,
-    socket: Arc<&UdpSocket>,
+    service_socket: Arc<UdpSocket>,
+    election_socket: Arc<UdpSocket>,
     stats: &Arc<Mutex<ServerStats>>,
 ) {
     let request: &str = std::str::from_utf8(buffer).expect("Failed to convert to UTF-8");
@@ -246,26 +269,42 @@ async fn handle_request(
         Ok(msg) => msg,
         Err(_) => return,
     };
-    println!(" Received message :\n{}", request);
+    println!("Received message :\n{}", request);
 
     match msg.msg_type {
         Type::ClientRequest(req_id) => {
-            handle_client(req_id, msg, socket, stats.to_owned(), src_addr).await;
+            handle_client(
+                req_id,
+                msg,
+                service_socket,
+                election_socket,
+                stats.to_owned(),
+                src_addr,
+            )
+            .await;
         }
         Type::ElectionRequest(priority) => {
             println!("handling election!");
             let req_id = msg.payload.clone().unwrap();
-            handle_election(priority, req_id, src_addr, socket, stats.to_owned()).await;
+            handle_election(
+                priority,
+                req_id,
+                src_addr,
+                service_socket,
+                election_socket,
+                stats.to_owned(),
+            )
+            .await;
         }
         Type::CoordinatorBrdCast(coordinator_ip) => {
             println!("Handlign Broadcast!");
             let req_id = msg.payload.clone().unwrap();
-            handle_coordinator(stats.to_owned(), coordinator_ip, socket, req_id).await;
+            handle_coordinator(stats.to_owned(), req_id).await;
         }
         Type::OKMsg(priority) => {
             println!("Handling OK");
             let req_id = msg.payload.clone().unwrap();
-            handle_ok_msg(req_id, src_addr, priority, socket, stats.to_owned()).await;
+            handle_ok_msg(req_id, stats.to_owned()).await;
         }
     }
 }
@@ -297,29 +336,39 @@ async fn main() {
 
     let service_socket = UdpSocket::bind(ip_service).await.unwrap();
     println!("Server (service) listening on {ip_service}");
+    let service_socket = Arc::new(service_socket);
+    let service_socket2 = Arc::clone(&service_socket);
 
     let election_socket = UdpSocket::bind(ip_elec).await.unwrap();
     println!("Server (election) listening on {ip_elec}");
+    let election_socket = Arc::new(election_socket);
+    let election_socket2 = Arc::clone(&election_socket);
 
     let stats = Arc::new(Mutex::new(stats));
-    let mut election_buffer = [0; 2048];
-    let mut service_buffer = [0; 2048];
     let stats_service = Arc::clone(&stats);
     let stats_election = Arc::clone(&stats);
+
+    let mut service_buffer: [u8; 2048] = [0; 2048];
+    let mut election_buffer: [u8; 2048] = [0; 2048];
 
     let h1 = tokio::spawn({
         async move {
             loop {
                 match service_socket.recv_from(&mut service_buffer).await {
                     Ok((bytes_read, src_addr)) => {
-                        let socket_clone = Arc::clone(&Arc::new(&service_socket));
-                        handle_request(
-                            &service_buffer[..bytes_read],
-                            src_addr,
-                            socket_clone,
-                            &stats_service,
-                        )
-                        .await;
+                        let service_socket = Arc::clone(&service_socket);
+                        let election_socket = Arc::clone(&election_socket2);
+                        let stats_clone = Arc::clone(&stats_service);
+                        tokio::spawn(async move {
+                            handle_request(
+                                &service_buffer[..bytes_read],
+                                src_addr,
+                                service_socket,
+                                election_socket,
+                                &stats_clone,
+                            )
+                            .await;
+                        });
                     }
                     Err(e) => {
                         eprintln!("Error receiving data: {}", e);
@@ -334,14 +383,19 @@ async fn main() {
             loop {
                 match election_socket.recv_from(&mut election_buffer).await {
                     Ok((bytes_read, src_addr)) => {
-                        let socket_clone = Arc::clone(&Arc::new(&election_socket));
-                        handle_request(
-                            &election_buffer[..bytes_read],
-                            src_addr,
-                            socket_clone,
-                            &stats_election,
-                        )
-                        .await;
+                        let service_socket = Arc::clone(&service_socket2);
+                        let election_socket = Arc::clone(&election_socket);
+                        let stats_clone = Arc::clone(&stats_election);
+                        tokio::spawn(async move {
+                            handle_request(
+                                &election_buffer[..bytes_read],
+                                src_addr,
+                                service_socket,
+                                election_socket,
+                                &stats_clone,
+                            )
+                            .await;
+                        });
                     }
                     Err(e) => {
                         eprintln!("Error receiving data: {}", e);
@@ -359,9 +413,9 @@ impl ServerStats {
     fn new() -> ServerStats {
         ServerStats {
             requests_buffer: HashMap::new(),
-            elections_initiated_by_me: HashMap::new(),
+            elections_initiated_by_me: HashSet::new(),
             elections_leader: HashMap::new(),
-            elections_received_oks: HashMap::new(),
+            elections_received_oks: HashSet::new(),
             running_elections: HashMap::new(),
             peer_servers: Vec::new(),
             sockets_ips: (String::new(), String::new()),
